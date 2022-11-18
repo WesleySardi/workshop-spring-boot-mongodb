166c522f004cbe3ffe61bd6a5074adf36a79db27
Wesley Sardi <wesley.sardi@gmail.com>
2022-11-18 11:49:24 -0300

    Obtendo um post por id.

---
+++ src/main/java/com/wesleysardi/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.wesleysardi.workshopmongo.services;
+
+import com.wesleysardi.workshopmongo.domain.Post;
+import com.wesleysardi.workshopmongo.domain.User;
+import com.wesleysardi.workshopmongo.dto.UserDTO;
+import com.wesleysardi.workshopmongo.repository.PostRepository;
+import com.wesleysardi.workshopmongo.repository.UserRepository;
+import com.wesleysardi.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
